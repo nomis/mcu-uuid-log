58562335b55f45ce9389ca17711c1da6f5cca34f
Simon Arlott <sa.me.uk>
2019-08-04 16:30:47 +0100

    Initial commit

---
+++ src/log.cpp
@@ -0,0 +1,391 @@
+/*
+ * uuid-log - Microcontroller logging framework
+ * Copyright 2019  Simon Arlott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <uuid/log.h>
+
+#include <Arduino.h>
+#include <stdarg.h>
+
+#include <list>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace uuid {
+
+namespace log {
+
+unsigned long Message::next_id_ = 0;
+std::map<Receiver*,Level> Logger::receivers_;
+Level Logger::level_ = Level::OFF;
+
+const char *pstr_level_uppercase_emerg __attribute__((__aligned__(sizeof(int)))) PROGMEM = "EMERG";
+const char *pstr_level_uppercase_crit __attribute__((__aligned__(sizeof(int)))) PROGMEM = "CRIT";
+const char *pstr_level_uppercase_alert __attribute__((__aligned__(sizeof(int)))) PROGMEM = "ALERT";
+const char *pstr_level_uppercase_err __attribute__((__aligned__(sizeof(int)))) PROGMEM = "ERR";
+const char *pstr_level_uppercase_warning __attribute__((__aligned__(sizeof(int)))) PROGMEM = "WARNING";
+const char *pstr_level_uppercase_notice __attribute__((__aligned__(sizeof(int)))) PROGMEM = "NOTICE";
+const char *pstr_level_uppercase_info __attribute__((__aligned__(sizeof(int)))) PROGMEM = "INFO";
+const char *pstr_level_uppercase_debug __attribute__((__aligned__(sizeof(int)))) PROGMEM = "DEBUG";
+const char *pstr_level_uppercase_trace __attribute__((__aligned__(sizeof(int)))) PROGMEM = "TRACE";
+const char *pstr_level_uppercase_off __attribute__((__aligned__(sizeof(int)))) PROGMEM = "OFF";
+
+const char *pstr_level_lowercase_emerg __attribute__((__aligned__(sizeof(int)))) PROGMEM = "emerg";
+const char *pstr_level_lowercase_crit __attribute__((__aligned__(sizeof(int)))) PROGMEM = "crit";
+const char *pstr_level_lowercase_alert __attribute__((__aligned__(sizeof(int)))) PROGMEM = "alert";
+const char *pstr_level_lowercase_err __attribute__((__aligned__(sizeof(int)))) PROGMEM = "err";
+const char *pstr_level_lowercase_warning __attribute__((__aligned__(sizeof(int)))) PROGMEM = "warning";
+const char *pstr_level_lowercase_notice __attribute__((__aligned__(sizeof(int)))) PROGMEM = "notice";
+const char *pstr_level_lowercase_info __attribute__((__aligned__(sizeof(int)))) PROGMEM = "info";
+const char *pstr_level_lowercase_debug __attribute__((__aligned__(sizeof(int)))) PROGMEM = "debug";
+const char *pstr_level_lowercase_trace __attribute__((__aligned__(sizeof(int)))) PROGMEM = "trace";
+const char *pstr_level_lowercase_off __attribute__((__aligned__(sizeof(int)))) PROGMEM = "off";
+
+std::string format_timestamp_ms(int days_width, uint64_t timestamp_ms) {
+	unsigned long days;
+	unsigned int hours, minutes, seconds, milliseconds;
+
+	days = timestamp_ms / 86400000UL;
+	timestamp_ms %= 86400000UL;
+
+	hours = timestamp_ms / 3600000UL;
+	timestamp_ms %= 3600000UL;
+
+	minutes = timestamp_ms / 60000UL;
+	timestamp_ms %= 60000UL;
+
+	seconds = timestamp_ms / 1000UL;
+	timestamp_ms %= 1000UL;
+
+	milliseconds = timestamp_ms;
+
+	std::vector<char> text(10 + 1 /* days */ + 2 + 1 /* hours */ + 2 + 1 /* minutes */ + 2 + 1 /* seconds */ + 3 /* milliseconds */ + 1);
+
+	snprintf(text.data(), text.size(), "%0*ld+%02d:%02d:%02d.%03d", days_width, days, hours, minutes, seconds, milliseconds);
+
+	return text.data();
+}
+
+char format_level_char(Level level) {
+	constexpr char log_level_chars[(int)Level::TRACE - (int)Level::OFF + 1] = { ' ', 'P', 'A', 'C', 'E', 'W', 'N', 'I', 'D', 'T' };
+	return log_level_chars[(int)level + 1];
+}
+
+const __FlashStringHelper *format_level_uppercase(Level level) {
+	const __FlashStringHelper *log_level_uppercase[(int)Level::TRACE - (int)Level::OFF + 1] = {
+			FPSTR(pstr_level_uppercase_off),
+			FPSTR(pstr_level_uppercase_emerg),
+			FPSTR(pstr_level_uppercase_crit),
+			FPSTR(pstr_level_uppercase_alert),
+			FPSTR(pstr_level_uppercase_err),
+			FPSTR(pstr_level_uppercase_warning),
+			FPSTR(pstr_level_uppercase_notice),
+			FPSTR(pstr_level_uppercase_info),
+			FPSTR(pstr_level_uppercase_debug),
+			FPSTR(pstr_level_uppercase_trace)
+	};
+	return log_level_uppercase[(int)level + 1];
+}
+
+const __FlashStringHelper *format_level_lowercase(Level level) {
+	const __FlashStringHelper *log_level_lowercase[(int)Level::TRACE - (int)Level::OFF + 1] = {
+			FPSTR(pstr_level_lowercase_off),
+			FPSTR(pstr_level_lowercase_emerg),
+			FPSTR(pstr_level_lowercase_crit),
+			FPSTR(pstr_level_lowercase_alert),
+			FPSTR(pstr_level_lowercase_err),
+			FPSTR(pstr_level_lowercase_warning),
+			FPSTR(pstr_level_lowercase_notice),
+			FPSTR(pstr_level_lowercase_info),
+			FPSTR(pstr_level_lowercase_debug),
+			FPSTR(pstr_level_lowercase_trace)
+	};
+	return log_level_lowercase[(int)level + 1];
+}
+
+Message::Message(uint64_t uptime_ms, Level level, const __FlashStringHelper *name, const std::string &&text)
+		: id_(next_id_++), uptime_ms_(uptime_ms), level_(level), name_(name), text_(std::move(text)) {
+}
+
+Logger::Logger(const __FlashStringHelper *name)
+		: name_(name) {
+
+};
+
+void Logger::register_receiver(Receiver *receiver, Level level) {
+	receivers_[receiver] = level;
+	refresh_log_level();
+};
+
+void Logger::unregister_receiver(Receiver *receiver) {
+	receivers_.erase(receiver);
+	refresh_log_level();
+};
+
+Level Logger::get_log_level(Receiver *receiver) {
+	const auto level = receivers_.find(receiver);
+
+	if (level != receivers_.end()) {
+		return level->second;
+	}
+
+	return Level::OFF;
+}
+
+void Logger::emerg(const char *format, ...) {
+	if (enabled(Level::EMERG)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::EMERG, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::emerg(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::EMERG)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::EMERG, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::crit(const char *format, ...) {
+	if (enabled(Level::CRIT)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::CRIT, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::crit(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::CRIT)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::CRIT, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::alert(const char *format, ...) {
+	if (enabled(Level::ALERT)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::ALERT, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::alert(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::ALERT)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::ALERT, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::err(const char *format, ...) {
+	if (enabled(Level::ERR)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::ERR, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::err(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::ERR)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::ERR, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::warning(const char *format, ...) {
+	if (enabled(Level::WARNING)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::WARNING, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::warning(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::WARNING)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::WARNING, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::notice(const char *format, ...) {
+	if (enabled(Level::NOTICE)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::NOTICE, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::notice(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::NOTICE)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::NOTICE, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::info(const char *format, ...) {
+	if (enabled(Level::INFO)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::INFO, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::info(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::INFO)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::INFO, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::debug(const char *format, ...) {
+	if (enabled(Level::DEBUG)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::DEBUG, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::debug(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::DEBUG)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::DEBUG, format, ap);
+		va_end(ap);
+	}
+};
+void Logger::trace(const char *format, ...) {
+	if (enabled(Level::TRACE)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::TRACE, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::trace(const __FlashStringHelper *format, ...) {
+	if (enabled(Level::TRACE)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(Level::TRACE, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::log(Level level, const char *format, ...) {
+	if (enabled(level)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(level, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::log(Level level, const __FlashStringHelper *format, ...) {
+	if (enabled(level)) {
+		va_list ap;
+
+		va_start(ap, format);
+		vlog(level, format, ap);
+		va_end(ap);
+	}
+};
+
+void Logger::vlog(Level level, const char *format, va_list ap) {
+	std::vector<char> text(MAX_LOG_LENGTH + 1);
+
+	if (vsnprintf(text.data(), text.size(), format, ap) <= 0) {
+		return;
+	}
+
+	dispatch(level, text);
+}
+
+void Logger::vlog(Level level, const __FlashStringHelper *format, va_list ap) {
+	std::vector<char> text(MAX_LOG_LENGTH + 1);
+
+	if (vsnprintf_P(text.data(), text.size(), reinterpret_cast<PGM_P>(format), ap) <= 0) {
+		return;
+	}
+
+	dispatch(level, text);
+}
+
+void Logger::dispatch(Level level, std::vector<char> &text) {
+	std::shared_ptr<Message> message = std::make_shared<Message>(get_uptime_ms(), level, name_, text.data());
+	text.resize(0);
+
+	for (auto &receiver : receivers_) {
+		if (level <= receiver.second) {
+			receiver.first->add_log_message(message);
+		}
+	}
+}
+
+inline bool Logger::enabled(Level level) {
+	return level <= level_;
+};
+
+void Logger::refresh_log_level() {
+	level_ = Level::OFF;
+
+	for (auto &receiver : receivers_) {
+		if (level_ < receiver.second) {
+			level_ = receiver.second;
+		}
+	}
+}
+
+} // namespace log
+
+} // namespace uuid
---
+++ src/uuid/log.h
@@ -0,0 +1,159 @@
+/*
+ * uuid-log - Microcontroller logging framework
+ * Copyright 2019  Simon Arlott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UUID_LOG_H_
+#define UUID_LOG_H_
+
+#include <Arduino.h>
+#include <stdarg.h>
+
+#include <list>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <uuid/common.h>
+
+namespace uuid {
+
+namespace log {
+
+enum class Level {
+	OFF = -1,
+	EMERG = 0,
+	ALERT,
+	CRIT,
+	ERR,
+	WARNING,
+	NOTICE,
+	INFO,
+	DEBUG,
+	TRACE
+};
+
+extern const char *pstr_level_uppercase_emerg;
+extern const char *pstr_level_uppercase_crit;
+extern const char *pstr_level_uppercase_alert;
+extern const char *pstr_level_uppercase_err;
+extern const char *pstr_level_uppercase_warning;
+extern const char *pstr_level_uppercase_notice;
+extern const char *pstr_level_uppercase_info;
+extern const char *pstr_level_uppercase_debug;
+extern const char *pstr_level_uppercase_trace;
+extern const char *pstr_level_uppercase_off;
+
+extern const char *pstr_level_lowercase_emerg;
+extern const char *pstr_level_lowercase_crit;
+extern const char *pstr_level_lowercase_alert;
+extern const char *pstr_level_lowercase_err;
+extern const char *pstr_level_lowercase_warning;
+extern const char *pstr_level_lowercase_notice;
+extern const char *pstr_level_lowercase_info;
+extern const char *pstr_level_lowercase_debug;
+extern const char *pstr_level_lowercase_trace;
+extern const char *pstr_level_lowercase_off;
+
+std::string format_timestamp_ms(int days_width, uint64_t timestamp_ms);
+char format_level_char(Level level);
+const __FlashStringHelper *format_level_uppercase(Level level);
+const __FlashStringHelper *format_level_lowercase(Level level);
+
+class Message {
+public:
+	Message(uint64_t uptime_ms, Level level, const __FlashStringHelper *name, const std::string &&text);
+	~Message() = default;
+
+	const unsigned long id_;
+	const uint64_t uptime_ms_;
+	const Level level_;
+	const __FlashStringHelper *name_;
+	const std::string text_;
+
+private:
+	static unsigned long next_id_;
+};
+
+class Receiver {
+public:
+	virtual ~Receiver() = default;
+
+	virtual void add_log_message(std::shared_ptr<Message> message) = 0;
+
+protected:
+	Receiver() = default;
+};
+
+class Logger {
+public:
+	static constexpr size_t MAX_LOG_LENGTH = 255;
+
+	Logger(const __FlashStringHelper *name);
+	~Logger() = default;
+
+	/**
+	 * Register a log receiver. Call again to change the log level.
+	 */
+	static void register_receiver(Receiver *receiver, Level level);
+	static void unregister_receiver(Receiver *receiver);
+
+	static Level get_log_level(Receiver *receiver);
+	/**
+	 * Cleanup processed messages. Must be called regularly to release memory.
+	 */
+	static void cleanup_messages();
+
+	static inline bool enabled(Level level);
+	void emerg(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void emerg(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void alert(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void alert(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void crit(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void crit(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void err(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void err(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void warning(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void warning(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void notice(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void notice(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void info(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void info(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void debug(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void debug(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void trace(const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void trace(const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void log(Level level, const char *format, ...) /* __attribute__ ((format (printf, 2, 3))) */;
+	void log(Level level, const __FlashStringHelper *format, ...) /* __attribute__ ((format (printf, 3, 4))) */;
+
+private:
+	static void refresh_log_level();
+	void vlog(Level level, const char *format, va_list ap);
+	void vlog(Level level, const __FlashStringHelper *format, va_list ap);
+	void dispatch(Level level, std::vector<char> &text);
+
+	static std::map<Receiver*,Level> receivers_;
+	static Level level_;
+
+	const __FlashStringHelper *name_;
+};
+
+} // namespace log
+
+} // namespace uuid
+
+#endif
