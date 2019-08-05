8d1996a1f1a70c92f684088a4b90578ee09d3f26
Simon Arlott <sa.me.uk>
2019-08-05 20:05:45 +0100

    Add unit test

---
+++ mock_include/Arduino.h
@@ -0,0 +1,32 @@
+/*
+ * uuid-log - Microcontroller logging framework
+ * Copyright 2019  Simon Arlott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARDUINO_H_
+#define ARDUINO_H_
+
+#define PROGMEM
+#define PGM_P const char *
+#define PSTR(s) (__extension__({static const char __c[] = (s); &__c[0];}))
+
+class __FlashStringHelper;
+#define FPSTR(string_literal) (reinterpret_cast<const __FlashStringHelper *>(string_literal))
+#define F(string_literal) (FPSTR(PSTR(string_literal)))
+
+#define vsnprintf_P vsnprintf
+
+#endif
---
+++ test/basic/main.cpp
@@ -0,0 +1,105 @@
+/*
+ * uuid-log - Microcontroller logging framework
+ * Copyright 2019  Simon Arlott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Arduino.h>
+#include <unity.h>
+
+#include <memory>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <uuid/log.h>
+
+class Test: public uuid::log::Receiver {
+public:
+	Test() = default;
+	~Test() override {
+		uuid::log::Logger::unregister_receiver(this);
+	}
+
+	void add_log_message(std::shared_ptr<uuid::log::Message> message) override {
+		message_ = message;
+	}
+
+	std::shared_ptr<uuid::log::Message> message_;
+};
+
+namespace uuid {
+
+uint64_t get_uptime_ms() {
+	static uint64_t millis = 0;
+	return ++millis;
+}
+
+} // namespace uuid
+
+void test() {
+	Test test1;
+	Test test2;
+	uuid::log::Logger logger{F("test"), uuid::log::Facility::LOCAL0};
+
+	uuid::log::Logger::register_receiver(&test1, uuid::log::Level::INFO);
+	logger.info("Hello, %u World!", 42);
+
+	TEST_ASSERT_TRUE_MESSAGE(test1.message_, "Receiver 1 must have the message");
+	TEST_ASSERT_EQUAL_UINT64((uint64_t)1, test1.message_->uptime_ms_);
+	TEST_ASSERT_EQUAL_INT(uuid::log::Level::INFO, test1.message_->level_);
+	TEST_ASSERT_EQUAL_INT(uuid::log::Facility::LOCAL0, test1.message_->facility_);
+	TEST_ASSERT_EQUAL_STRING("test", reinterpret_cast<const char *>(test1.message_->name_));
+	TEST_ASSERT_EQUAL_STRING("Hello, 42 World!", test1.message_->text_.c_str());
+	TEST_ASSERT_EQUAL_INT(1, test1.message_.use_count());
+
+	TEST_ASSERT_FALSE_MESSAGE(test2.message_, "Receiver 2 must not have the message");
+
+	test1.message_.reset();
+
+	uuid::log::Logger::register_receiver(&test1, uuid::log::Level::NOTICE);
+	logger.info("Hello, %u World!", 42);
+
+	TEST_ASSERT_FALSE_MESSAGE(test1.message_, "Receiver 1 must not have the message");
+	TEST_ASSERT_FALSE_MESSAGE(test2.message_, "Receiver 2 must not have the message");
+
+	uuid::log::Logger::register_receiver(&test1, uuid::log::Level::ALL);
+	uuid::log::Logger::register_receiver(&test2, uuid::log::Level::DEBUG);
+	logger.info("Hello, %u World!", 42);
+
+	TEST_ASSERT_TRUE_MESSAGE(test1.message_, "Receiver 1 must have the message");
+	TEST_ASSERT_EQUAL_UINT64((uint64_t)2, test1.message_->uptime_ms_);
+	TEST_ASSERT_EQUAL_INT(uuid::log::Level::INFO, test1.message_->level_);
+	TEST_ASSERT_EQUAL_INT(uuid::log::Facility::LOCAL0, test1.message_->facility_);
+	TEST_ASSERT_EQUAL_STRING("test", reinterpret_cast<const char *>(test1.message_->name_));
+	TEST_ASSERT_EQUAL_STRING("Hello, 42 World!", test1.message_->text_.c_str());
+	TEST_ASSERT_EQUAL_INT(2, test1.message_.use_count());
+
+	TEST_ASSERT_TRUE_MESSAGE(test2.message_, "Receiver 2 must have the message");
+	TEST_ASSERT_EQUAL_UINT64((uint64_t)2, test2.message_->uptime_ms_);
+	TEST_ASSERT_EQUAL_INT(uuid::log::Level::INFO, test2.message_->level_);
+	TEST_ASSERT_EQUAL_INT(uuid::log::Facility::LOCAL0, test2.message_->facility_);
+	TEST_ASSERT_EQUAL_STRING("test", reinterpret_cast<const char *>(test2.message_->name_));
+	TEST_ASSERT_EQUAL_STRING("Hello, 42 World!", test2.message_->text_.c_str());
+	TEST_ASSERT_EQUAL_INT(2, test2.message_.use_count());
+
+	TEST_ASSERT_TRUE_MESSAGE(test1.message_.get() == test2.message_.get(), "Message must be shared between receivers");
+}
+
+int main(int argc, char *argv[]) {
+	UNITY_BEGIN();
+	RUN_TEST(test);
+	return UNITY_END();;
+}
