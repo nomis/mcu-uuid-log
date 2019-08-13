03b99d463a491148fe5711aac73335bf8b9ce43c
Simon Arlott <sa.me.uk>
2019-08-13 20:28:48 +0100

    Make example lines short enough to fit on the platformio.org page

--- examples/Serial.cpp
+++ examples/Serial.cpp
@@ -21,9 +21,9 @@ public:
 	 */
 	void operator<<(std::shared_ptr<uuid::log::Message> message) {
 		Serial.printf_P(PSTR("%s %c [%S] %s\r\n"),
-				uuid::log::format_timestamp_ms(message->uptime_ms, 3).c_str(),
-				uuid::log::format_level_char(message->level),
-				message->name, message->text.c_str());
+			uuid::log::format_timestamp_ms(message->uptime_ms).c_str(),
+			uuid::log::format_level_char(message->level),
+			message->name, message->text.c_str());
 	}
 };
 
