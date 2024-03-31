e365f59142e756bf1eac9716d55b339bc4249c71
Simon Arlott <sa.me.uk>
2024-03-31 11:29:33 +0100

    add readthedocs config file

--- docs/conf.py
+++ docs/conf.py
@@ -3,13 +3,14 @@
 import os
 
 travis_ci = os.environ.get("TRAVIS") == 'true'
+rtd = os.environ.get("READTHEDOCS") == 'True'
 
 needs_sphinx = '1.3'
 extensions = []
 source_suffix = ['.rst']
 
 project = u'mcu-uuid-log'
-copyright = u'2019-2022, Simon Arlott'
+copyright = u'2019-2024, Simon Arlott'
 author = u'Simon Arlott'
 
 master_doc = 'index'
@@ -22,6 +23,9 @@ exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 highlight_language = 'c++'
 todo_include_todos = False
+
+if rtd:
+	html_theme = 'sphinx_rtd_theme'
 
 linkcheck_timeout = 60
 linkcheck_ignore = [r'https://github.com/.+/.+/(compare|commits)/.+']
